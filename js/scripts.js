838add89ed08c47afa9bf909546cc8b4f460f836
sadsdqdfqwe <130361061+sadsdqdfqwe@users.noreply.github.com>
2023-04-10 17:01:23 +0500

    Delete js directory

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.themes').addEventListener('change',(event)=>{
-	if (event.target.nodeName === 'INPUT'){
-		console.log('asdas');
-		document.documentElement.classList.remove('dark', 'light');
-		document.documentElement.classList.add(event.target.value);
-	}
-});		
